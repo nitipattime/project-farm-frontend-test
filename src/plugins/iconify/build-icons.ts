9150269d23dedb2be2cd5e0329f3efa337d69251
nitipattime <nitipattime@gmail.com>
2025-11-01 01:30:36 +0700

    fix code v3

--- src/plugins/iconify/build-icons.ts
+++ src/plugins/iconify/build-icons.ts
@@ -104,6 +104,13 @@ const sources: BundleScriptConfig = {
 }
 
 // File to save bundle to
+// const target = join(__dirname, 'icons.css')
+// import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// แปลง import.meta.url → __dirname
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const target = join(__dirname, 'icons.css')
 
 /**
